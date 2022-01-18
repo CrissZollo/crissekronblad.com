94f31aa506df6fc2e83a601dfd548271ed4727a3
Christoffer Kronblad <crisse.kronblad@gmail.com>
2022-01-18 21:23:22 +0100

    Added a new component: Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,18 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./components/home";
+
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Routes>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/users" element={<Users />}></Route>
-            <Route path="/" element={<Home />}></Route>
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
       </Router>
     </div>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return (
-    <>
-      <h2>Vilket djur röker mest?</h2>
-      <h2>- Kamel-jointen</h2>
-      <img
-        src="https://www.dexerto.com/wp-content/uploads/2021/04/28/el-risitas-spanish-laughing-guy-meme.jpg"
-        alt="laughing_img"
-        style={{ height: 500, width: 700 }}
-      />
-    </>
-  );
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
---
+++ src/components/home.jsx
@@ -0,0 +1,7 @@
+export default function home() {
+    return (
+        <div className="home">
+            <h1>Hi!</h1>
+        </div>
+    );
+}
