f079bf9d7feb0c5855e19e77db1197317117ff5f
Christoffer Kronblad <crisse@crisse-laptop.Ownit>
2022-08-14 12:51:53 +0200

    The user will now be treated by a large intro screen when entering the the website

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./navbar";
 import ProjectsCard from "./projectsCard";
 import Welcome from "./welcome";
+import Intro from "./intro";
 
 export default function Home() {
   return (
     <div className="home">
       <Navbar />
+      <Intro />
       <Welcome />
       <ProjectsCard />
     </div>
