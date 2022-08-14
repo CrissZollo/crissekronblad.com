440a1d9695dfd456559a85a3e857e89eb47516a9
Home_PC\Christoffer Kronblad <crisse.kronblad+sourcetree@gmail.com>
2022-08-14 21:03:27 +0200

    Fixed some overlapping issues with the intro text

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,8 +4,10 @@ import wavy from "../img/wavy_green.svg";
 export default function Intro() {
   return (
     <div className="intro-container">
-      <h1 id="intro-title">Hello World</h1>
-      <h3 id="intro-scrolldown-text">Scroll Down</h3>
+      <div className="into-title-container">
+        <h1 id="intro-title">Hello World</h1>
+        <h3 id="intro-scrolldown-text">Scroll Down</h3>
+      </div>
       <img className="wavy-full" src={wavy} alt="Wavy" />
     </div>
   );
